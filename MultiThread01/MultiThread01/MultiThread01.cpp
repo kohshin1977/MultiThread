907b17e41879ff89f0f9cb3e541003e168844d53
Kohshin Tokunaga <k-tokunaga@idk.co.jp>
2022-06-27 15:45:57 +0900

    第１０２章 マルチスレッド　その４コミット。

--- MultiThread01/MultiThread01/MultiThread01.cpp
+++ MultiThread01/MultiThread01/MultiThread01.cpp
@@ -11,13 +11,17 @@
 
 unsigned __stdcall mythread0(void *);
 unsigned __stdcall mythread1(void *);
-unsigned __stdcall mythread2(void *);
+
+int n;
+CRITICAL_SECTION cs;
 
 int main()
 {
 	int i;
-	HANDLE hTh[3];
-	unsigned int thID[3];
+	HANDLE hTh[2];
+	unsigned int thID[2];
+
+	InitializeCriticalSection(&cs);
 
 	hTh[0] = (HANDLE)_beginthreadex(
 		NULL,
@@ -27,7 +31,6 @@ int main()
 		CREATE_SUSPENDED,
 		&thID[0]
 	);
-
 	if (hTh[0] == 0) {
 		printf("スレッド0作成失敗\n");
 		return -1;
@@ -41,69 +44,63 @@ int main()
 		CREATE_SUSPENDED,
 		&thID[1]
 	);
-
 	if (hTh[1] == 0) {
 		printf("スレッド1作成失敗\n");
 		return -1;
 	}
 
-	hTh[2] = (HANDLE)_beginthreadex(
-		NULL,
-		0,
-		mythread2,
-		NULL,
-		CREATE_SUSPENDED,
-		&thID[2]
-	);
-
-	if (hTh[2] == 0) {
-		printf("スレッド2作成失敗\n");
-		return -1;
-	}
-
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 2; i++)
 		ResumeThread(hTh[i]);
 
-	for (i = 0; i < 10; i++)
-		printf("Main [%d]\n", i);
 
-	WaitForMultipleObjects(3, hTh, TRUE, INFINITE);
+	WaitForMultipleObjects(2, hTh, TRUE, INFINITE);
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 2; i++)
 		CloseHandle(hTh[i]);
+
+	DeleteCriticalSection(&cs);
 
 	return 0;
 }
 
 unsigned __stdcall mythread0(void *lpx)
 {
-	int i;
-	for (i = 0; i < 10; i++)
-		printf("子スレッド0[%d]\n", i);
+	int esc = 0;
 
-	printf("子スレッド0終了!\n");
+	while (1) {
+		EnterCriticalSection(&cs);
+		if (n < 20) {
+			printf("mythread0 n = %d\n", n);
+			n++;
+		}
+		else {
+			esc = 1;
+		}
+		LeaveCriticalSection(&cs);
+		if (esc)
+			break;
+	}
 
 	return 0;
 }
 
 unsigned __stdcall mythread1(void *lpx)
 {
-	int i;
-	for (i = 0; i < 20; i++)
-		printf("子スレッド1[%d]\n", i);
+	int esc = 0;
 
-	printf("子スレッド1終了!\n");
-
-	return 0;
-}
-
-unsigned __stdcall mythread2(void *lpx)
-{
-	int i;
-	for (i = 0; i < 5; i++)
-		printf("子スレッド2[%d]\n", i);
-
-	printf("子スレッド2終了!\n");
+	while (1) {
+		EnterCriticalSection(&cs);
+		if (n < 20) {
+			printf("mythread1 n = %d\n", n);
+			n++;
+		}
+		else {
+			esc = 1;
+		}
+		LeaveCriticalSection(&cs);
+		if (esc)
+			break;
+	}
 
 	return 0;
 }
