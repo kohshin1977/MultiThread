671c6f17665a4d4e2bcca2dcf6cff683f15fbe45
Kohshin Tokunaga <k-tokunaga@idk.co.jp>
2022-06-27 17:21:02 +0900

    第１０４章 マルチスレッド　その６コミット。

--- MultiThread01/MultiThread01/MultiThread01.cpp
+++ MultiThread01/MultiThread01/MultiThread01.cpp
@@ -8,23 +8,39 @@
 #include <stdio.h>
 #include <windows.h>
 #include <process.h>
+#include <conio.h>
+
+#define MUTEXNAME _T("MYMUTEX")
 
 unsigned __stdcall mythread0(void *);
 unsigned __stdcall mythread1(void *);
 
-HANDLE hEvent[3];
+HANDLE hEvent[2];
+
+BOOL bThEnd = FALSE;
+int i;
 
 int main()
 {
 	int i;
 	HANDLE hTh[2];
 	unsigned int thID[2];
+	HANDLE hMutex;
+
+	hMutex = CreateMutex(NULL, FALSE, MUTEXNAME);
+	if (hMutex == NULL) {
+		printf("ミューテックス作成失敗\n");
+		return -1;
+	}
+	hEvent[0] = CreateEvent(NULL, TRUE, FALSE, _T("CH0"));
+	hEvent[1] = CreateEvent(NULL, TRUE, FALSE, _T("CH1"));
+
 
 	hTh[0] = (HANDLE)_beginthreadex(
 		NULL,
 		0,
 		mythread0,
-		NULL,
+		&hMutex,
 		CREATE_SUSPENDED,
 		&thID[0]
 	);
@@ -37,7 +53,7 @@ int main()
 		NULL,
 		0,
 		mythread1,
-		NULL,
+		&hMutex,
 		CREATE_SUSPENDED,
 		&thID[1]
 	);
@@ -46,49 +62,66 @@ int main()
 		return -1;
 	}
 
-
-	hEvent[0] = CreateEvent(NULL, TRUE, FALSE, _T("CH0"));
-	hEvent[1] = CreateEvent(NULL, TRUE, FALSE, _T("CH1"));
-	hEvent[2] = CreateEvent(NULL, TRUE, FALSE, _T("MAINEVENT"));
-
 	//各スレッド実行開始
 	for (i = 0; i < 2; i++)
 		ResumeThread(hTh[i]);
 
-	printf("イベント0をシグナル状態にします\n");
-	SetEvent(hEvent[0]);
+	_getch();
+	bThEnd = TRUE;
 
-	WaitForSingleObject(hEvent[2], INFINITE);
+	WaitForMultipleObjects(2, hEvent, TRUE, INFINITE);
+	for (i = 0; i < 2; i++) {
+		if (CloseHandle(hTh[i])) {
+			printf("hTh[%d]のクローズに成功\n", i);
+		}
+		else {
+			printf("hTh[%d]のクローズ失敗\n", i);
+		}
+	}
+	if (CloseHandle(hMutex)) {
+		printf("ミューテックスハンドルのクローズに成功\n");
+	}
+	else {
+		printf("ミューテックスハンドルのクローズに失敗\n");
+	}
 	printf("親を終了します\n");
-	for (i = 0; i < 2; i++)
-		CloseHandle(hTh[i]);
-
 	return 0;
 }
 
 unsigned __stdcall mythread0(LPVOID lpx)
 {
-	int i;
+	HANDLE hM;
 
-	WaitForSingleObject(hEvent[0], INFINITE);
-	for (i = 0; i < 10; i++)
-		printf("子スレッド0[%d]\n", i);
+	hM = *(HANDLE *)lpx;
 
-	printf("イベント1をシグナル状態にします\n");
-	SetEvent(hEvent[1]);
-
+	while (!bThEnd) {
+		WaitForSingleObject(hM, INFINITE);
+		printf("スレッド０が%dを表示\n", i++);
+		ReleaseMutex(hM);
+	}
+	WaitForSingleObject(hM, INFINITE);
+	printf("スレッド０終了\n");
+	ReleaseMutex(hM);
+	SetEvent(hEvent[0]);                                \
 	return 0;
 }
 
 unsigned __stdcall mythread1(LPVOID lpx)
 {
-	int i;
+	HANDLE hM;
 
-	WaitForSingleObject(hEvent[1], INFINITE);
-	for (i = 0; i < 10; i++)
-		printf("子スレッド1[%d]\n", i);
+	hM = *(HANDLE *)lpx;
 
-	printf("イベント2をシグナル状態にします\n");
-	SetEvent(hEvent[2]);
+	while (!bThEnd) {
+		WaitForSingleObject(hM, INFINITE);
+		printf("スレッド１が%dを表示\n", i++);
+		ReleaseMutex(hM);
+	}
+	WaitForSingleObject(hM, INFINITE);
+	printf("スレッド１終了\n");
+	ReleaseMutex(hM);
+	SetEvent(hEvent[1]);
 	return 0;
+
 }
+
