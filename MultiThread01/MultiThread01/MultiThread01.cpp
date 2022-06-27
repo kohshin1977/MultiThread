7fdb5df72adcf7195e60698b1ed734f042d2b138
Kohshin Tokunaga <k-tokunaga@idk.co.jp>
2022-06-27 14:36:03 +0900

    GetExitCodeThread関数を使用してスレッドの終了を待つ。

--- MultiThread01/MultiThread01/MultiThread01.cpp
+++ MultiThread01/MultiThread01/MultiThread01.cpp
@@ -14,13 +14,28 @@ unsigned __stdcall mythread(void *);
 int main()
 {
 	unsigned int thID;
+	DWORD dwExCode;
+
 	HANDLE hTh;
 	int i;
 
 	hTh = (HANDLE)_beginthreadex(NULL, 0, mythread, NULL, 0, &thID);
+	if (hTh == 0) {
+		printf("スレッド作成失敗\n");
+		return -1;
+	}
+	for (i = 0; i < 10; i++)
+		printf("Main [%d]\n", i);
 
-	for (i = 0; i < 20; i++) {
-		printf("Mainです[%d]\n", i);
+
+	while (1) {
+		GetExitCodeThread(hTh, &dwExCode);
+		if (dwExCode == STILL_ACTIVE) {
+			printf("スレッド稼働中\n");
+		}
+		else {
+			break;
+		}
 	}
 
 	if (hTh != NULL) {
@@ -28,7 +43,7 @@ int main()
 		printf("ハンドルクローズしました\n");
 	}
 
-    return 0;
+	return 0;
 }
 
 unsigned __stdcall mythread(void *lpx)
